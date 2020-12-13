f61e1021c52b5292d03f195febe34c636ba4a7cc
liqiuhui <cala_li@163.com>
2020-12-14 00:20:20 +0800

    feat: 完成bundler

--- bundler.js
+++ bundler.js
@@ -1,8 +1,40 @@
 const fs = require('fs')
+const babelParser = require('@babel/parser')
+const babelTraverse = require('@babel/traverse').default
+
+let ID = 0
 
 function createAsset(filename) {
-    const file = fs.readFileSync(filename,{'encoding':'utf-8'})
-    console.log(file)
+    const content = fs.readFileSync(filename,{'encoding':'utf-8'})
+
+    const  ast = babelParser.parse(content,{
+        sourceType: "module"
+    })
+
+    const dependencies = []
+
+    babelTraverse(ast,{
+        ImportDeclaration:({node})=>{
+            dependencies.push(node.source.value)
+        }
+    })
+
+    
+
+    return {
+        id:ID++,
+        filename,
+        dependencies
+    }
+
 }
 
-createAsset('./example/entry.js')
+function createGraph(entry) {
+    const mainAsset =createAsset(entry)
+    return mainAsset
+}
+
+
+const graph = createGraph('./example/entry.js')
+
+console.log(graph);
